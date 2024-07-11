2e01703357d7cca108683c16e9cc55281d42abdd
MitVinay <vm05011996@gmail.com>
2024-07-11 16:05:57 +1000

    NER page completed

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import (
     Flask, url_for, render_template
 )
 
+from form import InputForm
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
@@ -11,9 +13,16 @@ app.config["SECRET_KEY"] = "secret_key"
 def home():
     return render_template("home.html", title = "Home")
 
-@app.route("/ner")
+@app.route("/ner", methods=["GET", "POST"])
 def ner():
-    pass
+    form = InputForm()
+    if form.validate_on_submit():
+        airline = [form.airline.data]
+        message = f"The Sentiment is {airline}"
+    else:
+        message = "Please provide valid input details!"
+    return render_template("ner.html", title = "Named Entity Recoginition", form=form,
+                           output=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- form.py
+++ form.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+import pandas as pd
+from wtforms import (
+    SubmitField,
+    TextAreaField
+)
+
 from wtforms.validators import DataRequired
 
-
-
-
 class InputForm(FlaskForm):
-    
 
+    airline = TextAreaField(
+        label = "Named Entity Recoginition",
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField(label="Predict")
